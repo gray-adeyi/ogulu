dfbd6b5f7f1f4679dd8f7eedd20e05e23fc08df6
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-12-11 02:55:51 +0100

    Debug collectstatic

--- ogulu/settings.py
+++ ogulu/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://damsky.herokuapp.com',]
+ALLOWED_HOSTS = ['https://damsky.herokuapp.com']
 
 
 # Application definition
